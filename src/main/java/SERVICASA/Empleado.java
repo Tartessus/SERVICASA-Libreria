5f1332f97901f8f635bdf23a9c1437c4d884d0b9
Tartessus <carloslbm@hotmail.com>
2022-05-31 19:48:59 +0200

    añado lista

--- src/main/java/SERVICASA/Empleado.java
+++ src/main/java/SERVICASA/Empleado.java
@@ -1,14 +1,19 @@
 package SERVICASA;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class Empleado {
-	
+
 	private int dni;
 	private String nombre;
-	private String ciudad;
+	private String apellido;
+	private String direccion;
 	private String email;
+	private String ciudad;
+	private String provincia;
+	private Collection<Servicio> servicios = new ArrayList<>();
 
-	
-	
 	public int getDni() {
 		return dni;
 	}
@@ -16,7 +21,7 @@ public class Empleado {
 	public void setDni(int dni) {
 		this.dni = dni;
 	}
-	
+
 	public String getNombre() {
 		return nombre;
 	}
@@ -40,23 +45,63 @@ public class Empleado {
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getProvincia() {
+		return provincia;
+	}
+
+	public void setProvincia(String provincia) {
+		this.provincia = provincia;
+	}
+
+	public Collection<Servicio> getServicios() {
+		return servicios;
+	}
+
+	public void setServicios(Collection<Servicio> servicios) {
+		this.servicios = servicios;
+	}
+
+	public void addServicio(Servicio servicio) {
+		getServicios().add(servicio);
+	}
 
 	public Empleado() {
 
 	}
 
-	public Empleado(int dni, String nombre, String ciudad, String email) {
-		this.dni =dni;
+	public Empleado(int dni, String nombre, String apellido, String direccion, String email, String ciudad, String provincia, Collection<Servicio> servicios ) {
+		this.dni = dni;
 		this.nombre = nombre;
-		this.ciudad = ciudad;
+		this.apellido = apellido;
+		this.direccion = direccion;
 		this.email = email;
+		this.ciudad = ciudad;
+		this.provincia = provincia;
+		this.servicios =servicios;
 
 	}
 
 	@Override
 	public String toString() {
-		return "Usuario: " + getNombre() + " con DNI: "+ getDni();
+		return "Usuario: " + getNombre() + " con DNI: " + getDni();
 	}
 
-
+	
 }
