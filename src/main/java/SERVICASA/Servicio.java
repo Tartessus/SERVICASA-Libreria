906af370e6c9afd29310b9c84a027d163dc82c54
Tartessus <carloslbm@hotmail.com>
2022-06-21 18:13:06 +0200

    ghjkl

--- src/main/java/SERVICASA/Servicio.java
+++ src/main/java/SERVICASA/Servicio.java
@@ -1,7 +1,15 @@
 package SERVICASA;
 
 public abstract class Servicio {
-	
+	private long id;
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
 	private String nombre;
 	private float precioBase;
 
